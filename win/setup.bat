53c8cf5360d02c0331e97238d77339f9bee7ea28
mitsuYashi <hm20011027@gmail.com>
2023-07-21 02:48:23 +0900

    initial commit

---
+++ win/setup.bat
@@ -0,0 +1,16 @@
+winget install Google.Chrome
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.VisualStudioCode
+winget install SlackTechnologies.Slack
+winget install Discord.Discord
+winget install Docker.DockerDesktop
+winget install Canonical.Ubuntu.2204
+winget install Git.Git
+winget install Google.AndroidStudio
+winget install LINE.LINE
+winget install Zoom.Zoom
+winget install 7zip.7zip
+winget install TeamViewer.TeamViewer
+
+echo "enable Hyper-V"
+call enable-Hyper-V.bat
